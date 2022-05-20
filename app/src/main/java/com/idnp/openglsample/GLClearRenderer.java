5c791218ce0495875b1d8efeac6d5bc5e6ebd8f2
Christian Dacid Sullca Puma <csullcap@unsa.edu.pe>
2022-05-20 00:42:35 -0500

    V1.0.1

--- app/src/main/java/com/idnp/openglsample/GLClearRenderer.java
+++ app/src/main/java/com/idnp/openglsample/GLClearRenderer.java
@@ -45,7 +45,6 @@ public class GLClearRenderer implements Renderer {
         gl.glClearDepthf(1.0f);
         gl.glEnable(GL10.GL_DEPTH_TEST);
         gl.glDepthFunc(GL10.GL_LEQUAL);
-        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
-                GL10.GL_NICEST);
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
     }
 }
--- app/src/main/java/com/idnp/openglsample/MainActivity.java
+++ app/src/main/java/com/idnp/openglsample/MainActivity.java
@@ -1,28 +1,21 @@
 package com.idnp.openglsample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import javax.microedition.khronos.opengles.GL10;
-
 public class MainActivity extends AppCompatActivity {
-    private MainView mView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         GLSurfaceView glView = new GLSurfaceView(this);
         glView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
